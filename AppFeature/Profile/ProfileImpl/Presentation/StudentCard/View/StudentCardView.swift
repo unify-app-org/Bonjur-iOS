5234cb6a08bf09609c2e198a4f5e4b6e8b7921bf
Hakim Aliyev <hliyev980@gmail.com>
2026-03-05 14:35:53 +0400

    made some studen card view ui

--- AppFeature/Profile/ProfileImpl/Presentation/StudentCard/View/StudentCardView.swift
+++ AppFeature/Profile/ProfileImpl/Presentation/StudentCard/View/StudentCardView.swift
@@ -7,11 +7,91 @@
 
 import SwiftUI
 import AppFoundation
+import AppUIKit
 
 struct StudentCardView: View {
     @ObservedObject var store: StoreOf<StudentCardFeature>
-    
-    var body: some View {
-        Text("StudentCard")
+  
+    var selectedColor: Color {
+        store.state.selectedCover?.bgColor ?? Color.Palette.primary
     }
+    var body: some View {
+        GeometryReader { proxy in
+            VStack(spacing: 16) {
+                topBarView(safeAreaTop:0)
+
+                if store.state.isChooseColorSheetPresented {
+                    cardView
+                    Spacer()
+                } else {
+                    Spacer()
+                    cardView
+                    Spacer()
+                    Spacer()
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            .padding(.horizontal, 16)
+            .animation(.easeInOut(duration: 0.25), value: store.state.isChooseColorSheetPresented)
+        }
+        .background(
+            
+            LinearGradient(
+                colors: [
+                    selectedColor.opacity(0.5),
+                    Color.Palette.white
+                ],
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
+        )
+    }
+
+    @ViewBuilder
+    private var cardView: some View {
+        if let model = store.state.previewCard {
+            UserCardView(model: model, onTap: {})
+                .fixedSize(horizontal: false, vertical: true)
+                .allowsHitTesting(false)
+        }
+    }
+
+    private func topBarView(safeAreaTop: CGFloat) -> some View {
+        VStack(alignment: .leading, spacing: 16) {
+            customNavigationBar(safeAreaTop: safeAreaTop)
+            titleView
+        }
+    }
+
+    private func customNavigationBar(safeAreaTop: CGFloat) -> some View {
+        HStack {
+            Button {
+                store.send(.closeTapped)
+            } label: {
+                Image(uiImage: UIImage.Icons.xmark)
+            }
+            Spacer()
+            
+            Button {
+                store.send(.editTapped)
+            } label: {
+                Image(uiImage: UIImage.Icons.penLine)
+            }
+        }
+      
+        .padding(.top, safeAreaTop)
+        .padding(.bottom, 8)
+       
+    }
+    private var titleView: some View {
+        HStack {
+            Text("User Card")
+                .font(Font.Typography.TitleL.extraBold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+               
+        }
+      
+    }
+    
 }

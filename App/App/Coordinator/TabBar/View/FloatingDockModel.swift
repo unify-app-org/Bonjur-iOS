a77088e3efa1805634bda39a6a08363811cd060b
Hakim Aliyev <hliyev980@gmail.com>
2026-04-16 01:29:24 +0400

    added animated naviagtion dock

--- App/App/Coordinator/TabBar/View/FloatingDockModel.swift
+++ App/App/Coordinator/TabBar/View/FloatingDockModel.swift
@@ -17,6 +17,7 @@ final class FloatingDockModel: ObservableObject {
     @Published var mode: FloatingDockMode = .home
     @Published var joinedEventsCount: Int = 0
     @Published var joinedHangoutsCount: Int = 0
+    @Published var isVisible = true
     @Published var isActivitiesPresented = false
     @Published var isCreatePresented = false
     
